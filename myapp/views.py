a2ce8c34a7e79c28b76e28ccca0cb4af8c4388c7
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2022-08-26 14:02:18 -0700

    Added views for index and products on myapp with their templates.

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Product
+
 
 # Create your views here.
 def index(request):
     return render(request, 'myapp/index.html')
+
+
+def products(request):
+    product_list = Product.objects.all()
+    return render(request, 'myapp/products.html', {'products': product_list})
