74f4f65a9e52c1ff632f19f6bb38634aff35e674
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2022-08-26 14:10:19 -0700

    Added product detail view and url path.

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('products/', views.products, name='products'),
+    path('products/<int:id>/', views.product_detail, name='product_detail'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -10,3 +10,8 @@ def index(request):
 def products(request):
     product_list = Product.objects.all()
     return render(request, 'myapp/products.html', {'products': product_list})
+
+
+def product_detail(request, id):
+    product = Product.objects.get(id=id)
+    return render(request, 'myapp/product_detail.html', {'product': product})
