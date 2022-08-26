7c1bf3c460dd4b6072f3d880834ba830eaff5e9f
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2022-08-26 14:20:49 -0700

    Added namespaces no myapp urls.py file.

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'myapp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('products/', views.products, name='products'),
